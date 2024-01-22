9ee8d2ce751d2b0afa41923fd2049a760fb4e87d
COOPSPROFI <SanyaYoutube1@mail.ru>
2024-01-23 02:40:45 +0300

    some updates

---
+++ cmd/sso/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"github.com/COOPSPROFI/ScratchGolangGRPC/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	// TODO: init config
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
+
+	// TODO: init logger (slog)
+	log := setupLogger(cfg.Env)
+	log.Info("starting application", slog.String("env", cfg.Env))
+
+	// TODO: init application (app)
+
+	// TODO: serve GRPC-server application
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
+}
---
+++ internal/grpc/auth/server.go
@@ -0,0 +1,7 @@
+package auth
+
+import ssov1 from "github.com/COOPSPROFI/protosAuth/get/go/sso"
+
+type serverAPI struct {
+	ssov1.
+}
